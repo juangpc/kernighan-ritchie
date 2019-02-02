49c0ee431b08de4ee89d072a724da8535e9e3871
jgpc <juangpc@gmail.com>
2019-02-02 01:22:05 +0100

    chap1e5.c

--- chap1/chap1e5.c
+++ chap1/chap1e5.c
@@ -6,7 +6,7 @@ int main(void) {
   printf("Fahr - Celsius\n");
   printf("--------------\n");
 
-  for(fahr = 0; fahr <= 300; fahr = fahr + 20) {
+  for(fahr = 300; fahr >= 0; fahr = fahr - 20) {
     celsius = (5.0/9.0) * (fahr-32);
     printf("%6.2f\t%6.2f\n",fahr,celsius);
   }
